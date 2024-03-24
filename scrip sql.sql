d0643ce849efbd00ba397006f7a3caab8540952c
DESKTOP-M8JOCQL\angel <angela.horta.osorio@gmail.com>
2024-03-24 15:39:41 -0300

    ultima version

--- scrip sql.sql
+++ scrip sql.sql
@@ -1,3 +1,8 @@
+-- CREATE SCHEMA
+CREATE SCHEMA alkawallet;
+
+USE alkawallet;
+
 -- creacion BD tabla usuario
 CREATE TABLE usuario (
   user_id INT NOT NULL AUTO_INCREMENT,
@@ -6,6 +11,13 @@ CREATE TABLE usuario (
   contrasenia VARCHAR(45) NULL,
   saldo INT NULL,
   PRIMARY KEY (user_id));
+  
+-- creacion Moneda
+CREATE TABLE moneda (
+ currency_id INT NOT NULL AUTO_INCREMENT,
+ currency_name VARCHAR(100) NULL,
+ currency_symbol VARCHAR(3) NULL,
+ PRIMARY KEY (currency_id));
   
 -- creacion BD Transaccion
 CREATE TABLE transaccion (
@@ -18,31 +30,13 @@ CREATE TABLE transaccion (
   INDEX user_sender_idx (sender_user_id ASC) VISIBLE,
   INDEX user_receiver_idx (receiver_user_id ASC) VISIBLE,
   INDEX origen_currency_idx (origin_currency_id ASC) VISIBLE,
-  CONSTRAINT user_sender
     FOREIGN KEY (sender_user_id)
-    REFERENCES usuario (user_id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT user_receiver
+    REFERENCES usuario (user_id),
     FOREIGN KEY (receiver_user_id)
-    REFERENCES usuario (user_id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT origen_currency
+    REFERENCES usuario (user_id),
     FOREIGN KEY (origin_currency_id)
-    REFERENCES moneda (currency_id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
+    REFERENCES moneda (currency_id));
 	
--- creacion Moneda
-	CREATE TABLE moneda (
-	currency_id INT NOT NULL AUTO_INCREMENT,
-	currency_name VARCHAR(100) NULL,
-	currency_symbol VARCHAR(3) NULL,
-	PRIMARY KEY (currency_id));
-  
-  
 -- poblacion de tablas usuario
 INSERT INTO usuario (nombre, correo_electronico, contrasenia, saldo) 
 VALUES ('sofia', 'sofia@gmail.com', '123', '500'),
@@ -55,19 +49,6 @@ VALUES ('sofia', 'sofia@gmail.com', '123', '500'),
 ('paz', 'paz@gmail.com', '123', '39'),
 ('ruben', 'ruben@gmail.com', '123', '70'),
 ( 'ester', 'ester@gmail.com', '123', '29');
-
--- poblacion transacciones
-INSERT INTO transaccion (sender_user_id, receiver_user_id, transaction_date, origin_currency_id) 
-VALUES ('1', '2', '2024-03-01', '1'),
-('3', '1', '2024-03-15', '1'),
-('1', '1', '2024-03-18', '1'),
-('2', '3', '2024-03-18', '2'),
-('3', '1', '2024-03-20', '2'),
-('1', '2', '2024-03-20', '3'),
-('2', '3', '2024-03-25', '4'),
-('3', '1', '2024-03-26', '5'),
-('1', '2', '2024-03-29', '10'),
-( '2', '3', '2024-03-30', '6');
 
 -- poblacion moneda		
 INSERT INTO moneda (currency_name, currency_symbol) 
@@ -82,10 +63,41 @@ VALUES ('peso chileno', '$'),
 ('real brasileño', 'BRL'),
 ( 'corona noruega', 'NOK');
 
+-- poblacion transacciones
+INSERT INTO transaccion (sender_user_id, receiver_user_id, transaction_date, origin_currency_id) 
+VALUES ('1', '2', '2024-03-01', '1'),
+('3', '1', '2024-03-15', '1'),
+('1', '2', '2024-03-18', '1'),
+('2', '3', '2024-03-18', '2'),
+('3', '1', '2024-03-20', '2'),
+('1', '2', '2024-03-20', '3'),
+('2', '3', '2024-03-25', '4'),
+('3', '1', '2024-03-26', '5'),
+('1', '2', '2024-03-29', '10'),
+( '2', '3', '2024-03-30', '6');
+
+-- Verificacion de tablas y datos creados
+-- SELECT * FROM usuario
+-- SELECT * FROM moneda
+-- SELECT * FROM transaccion
+
 -- Consulta para obtener el nombre de la moneda elegida por un usuario específico
+-- Respuesta con selec anidado
 select currency_name from moneda 
 where currency_id in (select origin_currency_id from transaccion where sender_user_id = 1);
 
+--Respuesta con Join 
+SELECT 
+    mo.currency_name
+FROM
+    moneda mo
+        JOIN
+    transaccion tra ON tra.origin_currency_id = mo.currency_id
+        JOIN
+    usuario usu ON usu.user_id = tra.sender_user_id
+WHERE
+    usu.user_id = 1;
+	
 -- Consulta para obtener todas las transacciones registradas
 SELECT * FROM transaccion;
 
@@ -93,7 +105,11 @@ SELECT * FROM transaccion;
 select * from transaccion where sender_user_id = 1;
 
 -- Sentencia DML para modificar el campo correo electrónico de un usuario específico
+-- Verificar datos antes del cambio SELECT correo_electronico FROM usuario where user_id = 1;
 update usuario set correo_electronico = 'sofia@yahoo.com' where user_id = 1;
+--Verificacion del cambio correo electronico SELECT correo_electronico FROM usuario where user_id = 1;
 
 -- Sentencia para eliminar los datos de una transacción (eliminado de la fila completa)
+-- Verificar datos antes del cambio SELECT * FROM transaccion where transaction_id = 4;
 delete from transaccion where transaction_id = 4
+-- Verificar datos despues del eliminacion SELECT * FROM transaccion where transaction_id = 4;
